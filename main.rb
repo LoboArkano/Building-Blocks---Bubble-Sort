53b7adf9c63a585b81a1496f8b0ad022e64a7d12
LoboArkano <pipiolo_mananes@hotmail.com>
2020-04-13 12:25:24 -0500

    Fix rubocop errors

--- main.rb
+++ main.rb
@@ -1,39 +1,42 @@
-def bubble_sort(array) 
-  return array if array.length <= 1
+def bubble_sort(array)
+  if array.length <= 1
+    return array
+  end
+
   previous_index = 0
   next_index = 1
   0.upto(array.length - 1) do
-    array.each_with_index do |x, index|
+    array.each_with_index do |_x, index|
       previous_index = index
       next_index = index + 1
       break if next_index == array.length
+
       if array[previous_index] > array[next_index]
-        array[previous_index],array[next_index] = array[next_index], array[previous_index]
+        array[previous_index], array[next_index] = array[next_index], array[previous_index]
       end
     end
   end
   return array
 end
 
-def bubble_sort_by (array)
-
-  0.upto(array.length - 1) do |loop_index|
-    array.each_with_index do | x, index |
-      previous_index = index
+def bubble_sort_by(array)
+  0.upto(array.length - 1) do
+    array.each_with_index do |_x, index|
       next_index = index + 1
       break if next_index == array.length
-      if yield(array[index], array[index+1]) > 0
-        array[index], array[index+1] = array[index+1], array[index]
+
+      if yield(array[index], array[index + 1]).positive?
+        array[index], array[index + 1] = array[index + 1], array[index]
       end
     end
   end
   puts array
 end
 
-print bubble_sort([6,2,3,7,4,8,5])
+print bubble_sort([6, 2, 3, 7, 4, 8, 5])
 
-puts 
+puts
 
-bubble_sort_by(["hi","hello", "y","hey"]) do |left,right|
+bubble_sort_by(%w(hi hello y hey)) do |left,right|
   left.length - right.length
 end
