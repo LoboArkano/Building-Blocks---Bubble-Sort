cf0bd7370cbee4a8f5aa4e8515e15a303ccd18e9
LoboArkano <pipiolo_mananes@hotmail.com>
2020-04-13 12:38:35 -0500

    Fix rubocop errors

--- main.rb
+++ main.rb
@@ -1,22 +1,22 @@
 def bubble_sort(array)
   if array.length <= 1
     return array
-  end
+  else
+    previous_index = 0
+    next_index = 1
+    0.upto(array.length - 1) do
+      array.each_with_index do |_x, index|
+        previous_index = index
+        next_index = index + 1
+        break if next_index == array.length
 
-  previous_index = 0
-  next_index = 1
-  0.upto(array.length - 1) do
-    array.each_with_index do |_x, index|
-      previous_index = index
-      next_index = index + 1
-      break if next_index == array.length
-
-      if array[previous_index] > array[next_index]
-        array[previous_index], array[next_index] = array[next_index], array[previous_index]
+        if array[previous_index] > array[next_index]
+          array[previous_index], array[next_index] = array[next_index], array[previous_index]
+        end
       end
     end
+    return array
   end
-  return array
 end
 
 def bubble_sort_by(array)
@@ -25,18 +25,22 @@ def bubble_sort_by(array)
       next_index = index + 1
       break if next_index == array.length
 
-      if yield(array[index], array[index + 1]).positive?
+      if array && yield(array[index], array[index + 1]).positive?
         array[index], array[index + 1] = array[index + 1], array[index]
       end
     end
   end
-  puts array
+  return array
 end
 
 print bubble_sort([6, 2, 3, 7, 4, 8, 5])
 
 puts
 
-bubble_sort_by(%w(hi hello y hey)) do |left,right|
+arr = bubble_sort_by(%w[hi hello y hey]) do |left,right|
   left.length - right.length
 end
+
+print arr
+
+puts
